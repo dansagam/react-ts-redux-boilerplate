103011bfb267b002acc67e4fde8dbf3b53e23e0b
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2022-09-29 13:50:12 +0100

    feat: set up the app

--- src/views/auth/routes/index.tsx
+++ src/views/auth/routes/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "views/auth/pages/SignIn";
 
 function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<div>Auth</div>} />
+      <Route path="/" element={<div>Auth</div>}>
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="login" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 }
