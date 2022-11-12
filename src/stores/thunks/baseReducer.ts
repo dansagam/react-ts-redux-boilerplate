9352ac791d320ee832c0898f7ebcb8b9eb91599b
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2022-11-12 08:11:36 +0100

    fix: the base reducer

--- src/stores/thunks/baseReducer.ts
+++ src/stores/thunks/baseReducer.ts
@@ -20,27 +20,52 @@ export const pending =
   (state: T) => {
     state.loading = type || LOADING.PENDING;
   };
-// type GG = <T>() => Extract<T, BaseAppStoreState>;
+
+export type DD = <T>() => Extract<T, BaseAppStoreState>;
+
 export const fulfilled =
-  <T extends BaseAppStoreState>(actionType: keyof T) =>
+  <T extends BaseAppStoreState>(actionType: keyof Omit<T, keyof BaseAppStoreState>) =>
   (state: T, action: PayloadAction<T[typeof actionType]>) => {
     state.loading = LOADING.IDLE;
     state.error = null;
     state[actionType] = action.payload;
   };
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+const withBase = <T extends BaseAppStoreState>() => {
+  return (x: keyof Omit<T, keyof BaseAppStoreState>) => ({ data: typeof x });
+};
+
+// const hhf = withBase<Record<string, string>>();
+// typeof hhf("string").data;
+declare function f1<T extends BaseAppStoreState>(
+  x: keyof Omit<T, keyof BaseAppStoreState>
+): { data: T[typeof x] };
 
 export const fulfilledTest =
-  <T extends BaseAppStoreState>(actionType: keyof T) =>
-  (state: T, action: PayloadAction<T[typeof actionType]>) => {
+  <T extends BaseAppStoreState>(actionType: keyof Omit<T, keyof BaseAppStoreState>) =>
+  (state: T, action: PayloadAction<ReturnType<typeof f1<T>>>) => {
     state.loading = LOADING.IDLE;
     state.error = null;
-    state[actionType] = action.payload;
+    state[actionType] = action.payload.data;
   };
 
 export const jhd = 333;
 
-interface GG extends BaseAppStoreState {
-  dd: string;
+// interface GG extends BaseAppStoreState {
+//   dd: string;
+// }
+
+export type Dok<K extends BaseAppStoreState> = { [Property in keyof K]: K[Property] };
+
+interface Gf extends BaseAppStoreState {
+  milk: string;
 }
 
-fulfilled<GG>("dd");
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+const FO: Dok<Gf> = {
+  milk: "d",
+  status: "",
+  loaded: [],
+  loading: "finished",
+  error: null,
+};
